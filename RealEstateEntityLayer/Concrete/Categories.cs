f7c5238dc2b254a0ccea8346e9bf3db8b2b02751
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-08 20:39:01 +0300

    Lecture 107 - Kategori Sınıfının Oluşturulması

---
+++ RealEstateEntityLayer/Concrete/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateEntityLayer.Concrete
+{
+    public class Categories
+    {
+        public int ctg_id { get; set; }
+        public string ctg_name { get; set; }
+        public string ctg_details { get; set; }
+        public bool ctg_status { get; set; }
+    }
+}
