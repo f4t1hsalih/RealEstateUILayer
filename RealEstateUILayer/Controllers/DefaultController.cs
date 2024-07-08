b7cdda09aa92244b69dcc6d1274b298d34efe14b
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-08 20:08:21 +0300

    Lecture 105 - Register Formu

--- RealEstateUILayer/Controllers/DefaultController.cs
+++ RealEstateUILayer/Controllers/DefaultController.cs
@@ -22,5 +22,9 @@ namespace RealEstateUILayer.Controllers
         {
             return View();
         }
+        public ActionResult Index5()
+        {
+            return View();
+        }
     }
 }
