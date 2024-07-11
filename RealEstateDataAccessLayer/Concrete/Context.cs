20610c10be17396afaa180ef8ac3674c347e2a25
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-11 17:20:11 +0300

    Lecture 109 - Context Sınıfı

---
+++ RealEstateDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,10 @@
+﻿using RealEstateEntityLayer.Concrete;
+using System.Data.Entity;
+
+namespace RealEstateDataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categories> tbl_categories { get; set; }
+    }
+}
--- RealEstateEntityLayer/Concrete/Categories.cs
+++ RealEstateEntityLayer/Concrete/Categories.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RealEstateEntityLayer.Concrete
 {
     public class Categories
     {
+        [Key]
         public int ctg_id { get; set; }
         public string ctg_name { get; set; }
         public string ctg_details { get; set; }
