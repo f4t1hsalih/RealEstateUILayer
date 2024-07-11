571c4afbb96cd95b32791755ee8a7ece36f92875
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-11 18:56:38 +0300

    Lecture 115 - Category Repository

---
+++ RealEstateDataAccessLayer/Repository/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using RealEstateDataAccessLayer.Abstract;
+using RealEstateDataAccessLayer.Concrete;
+using RealEstateEntityLayer.Concrete;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace RealEstateDataAccessLayer.Repository
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context con = new Context();
+        DbSet<Categories> _category;
+        public void Delete(Categories t)
+        {
+            _category.Remove(t);
+            con.SaveChanges();
+        }
+
+        public Categories GetById(int id)
+        {
+            return _category.Find(id);
+        }
+
+        public List<Categories> GetListAll()
+        {
+            return _category.ToList();
+        }
+
+        public void Insert(Categories t)
+        {
+            _category.Add(t);
+            con.SaveChanges();
+        }
+
+        public void Update(Categories t)
+        {
+            con.SaveChanges();
+        }
+    }
+}
---
+++ RealEstateDataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using RealEstateDataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+
+namespace RealEstateDataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T>
+    {
+        public void Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
