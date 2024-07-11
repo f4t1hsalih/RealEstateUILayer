e8f02e5fc719a22d24f446ad651af7d78e2afc62
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-11 17:32:32 +0300

    Lecture 111 - Migration İşlemi

---
+++ RealEstateDataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RealEstateDataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RealEstateDataAccessLayer.Concrete.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(RealEstateDataAccessLayer.Concrete.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
